9e6a2aa094138b45d837a089182fe1d03862a2a5
Dimitar Keremedchiev <d.k.keremedchiev@gamil.com>
2014-09-21 22:21:02 +0300

    Defining Classes Homework

---
+++ 01.Defining-Classes/1.Persons/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+internal class Person
+{
+    private string name;
+    private int age;
+    private string email;
+
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            while (string.IsNullOrEmpty(value))
+            {
+                Console.Write("\"Name\" Cannot be empty, please enter your name: ");
+                value = Console.ReadLine();
+            }
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            while (value < 8 || value > 100)
+            {
+                Console.Write("\"Age\" Should be in range [8...100], please enter your age: ");
+                value = int.Parse(Console.ReadLine());
+            }
+            this.age = value;
+        }
+    }
+
+    public string Email
+    {
+        get { return this.email; }
+
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                while (value != null && !value.Contains("@"))
+                {
+                    Console.Write("\"Email\" incorrect format, please enter your email: ");
+                    value = Console.ReadLine();
+                }
+                this.email = "\nemail " + value;
+            }
+            else
+            {
+                this.email = "\nemail - no email";
+            }
+        }
+    }
+
+    public Person(string name, int age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public override string ToString()
+    {
+        string result = string.Format("Person:\nName - {0}\nAge - {1}{2}", name, age, email);
+        return result;
+    }
+}
---
+++ 01.Defining-Classes/1.Persons/Persons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Persons
+{
+    private static void Main()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Please enter your age: ");
+        byte age = byte.Parse(Console.ReadLine());
+        Console.Write("Please enter your email: ");
+        string email = Console.ReadLine();
+
+        Person somePerson = new Person(name, age, email);
+
+        Console.WriteLine(somePerson);
+    }
+}
