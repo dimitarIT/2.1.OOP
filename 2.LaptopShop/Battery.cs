8cc604dd5225834ad052aab20d46040c2ff420f7
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-09-22 10:12:09 +0300

    Defining Classes Homework

---
+++ 2.LaptopShop/Battery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Battery
+{
+    private string description;
+    private float lifeInHours;
+
+    public Battery(float lifeInHours, string description = null)
+    {
+        this.LifeInHours = lifeInHours;
+        this.Description = description;
+    }
+
+    public string Description
+    {
+        get { return this.description; }
+        set
+        {
+            if (value != null && value.Length < 1)
+            {
+                throw new ArgumentNullException(Description);
+            }
+            else
+            {
+                this.description = value;
+            }
+        }
+    }
+
+    public float LifeInHours
+    {
+        get { return this.lifeInHours; }
+        set
+        {
+            if (value < 1f)
+            {
+                throw new ArgumentNullException("LifeInHours");
+            }
+            else
+            {
+                this.lifeInHours = value;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = string.Format
+            ("{0}, Battery life in hours: {1}",
+            this.Description, this.LifeInHours);
+        return result;
+    }
+}
