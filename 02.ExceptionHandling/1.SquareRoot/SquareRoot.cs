e5499d0eaeb912bdc7909a4d03ee7bd133c49ff6
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-09-24 11:19:45 +0300

    Exception Handling Homework

---
+++ 02.ExceptionHandling/1.SquareRoot/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1.SquareRoot
+{
+    internal class SquareRoot
+    {
+        private static void Main()
+        {
+            try
+            {
+                Console.Write("Please enter an integer number: ");
+                uint number = uint.Parse(Console.ReadLine());
+                double sqrt = Math.Sqrt(number);
+                Console.WriteLine(sqrt);
+            }
+            catch
+                (FormatException)
+            {
+                Console.Error.WriteLine("Invalid number");
+            }
+            catch (OverflowException)
+            {
+                Console.Error.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
