ec439ad8b4a659fe92e3bff546b654050fe05502
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-09-25 23:16:48 +0300

    Static Members and Namespaces Homework

---
+++ 03.Static-Members-and-Namespaces/1.2.3.Point3D-DistanceColculator-Paths/Path3D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+internal class Path3D
+{
+    private List<Point3D> points = new List<Point3D>();
+
+    public Path3D(params Point3D[] points)
+    {
+        if (points.Length > 0)
+        {
+            this.points.AddRange(points);
+        }
+    }
+
+    public void AddPoint(Point3D point)
+    {
+        this.points.Add(point);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(" ", this.points);
+    }
+}
