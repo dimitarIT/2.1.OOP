df49f8fab42d6c12d2647d7f3ed80146cc37f963
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-09-29 12:25:46 +0300

    Other Types in OOP Homework

---
+++ 04.Other-Types-in-OOP/02.FractionCalculator/Fraction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public struct Fraction
+{
+    private long numerator;
+
+    private long denominator;
+
+    public Fraction(long numerator, long denumerator)
+        : this()
+    {
+        this.Numerator = numerator;
+        this.Denominator = denumerator;
+    }
+
+    public long Numerator
+    {
+        get
+        {
+            return this.numerator;
+        }
+        set
+        {
+            this.numerator = value;
+        }
+    }
+
+    public long Denominator
+    {
+        get
+        {
+            return this.denominator;
+        }
+
+        set
+        {
+            if (value == 0)
+            {
+                throw new ArgumentException("Denominator can not be zero!");
+            }
+
+            if (value < 0)
+            {
+                value *= -1;
+                this.Numerator *= -1;
+            }
+
+            this.denominator = value;
+        }
+    }
+
+    public static Fraction operator -(Fraction a, Fraction b)
+    {
+        checked
+        {
+            long newNumerator = a.numerator * b.denominator - b.numerator * a.denominator;
+            long newDenominator = a.denominator * b.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+    }
+
+    public static Fraction operator +(Fraction a, Fraction b)
+    {
+        checked
+        {
+            long newNumerator = a.numerator * b.denominator + b.numerator * a.denominator;
+            long newDenominator = a.denominator * b.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0}", (decimal)this.Numerator / this.Denominator);
+    }
+}
